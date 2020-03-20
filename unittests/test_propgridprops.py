4636c22536be3891ff3770c53cb89a61d411369d
Per A. Brodtkorb <per.andreas.brodtkorb@gmail.com>
2020-03-20 12:24:45 +0100

    Fix issue #1549: test_propgridprops12 fails on windows.

--- unittests/test_propgridprops.py
+++ unittests/test_propgridprops.py
@@ -1,14 +1,14 @@
+import os
 import unittest
 from unittests import wtc
 import wx.propgrid as pg
-
 #---------------------------------------------------------------------------
+
 
 class propgridprops_Tests(wtc.WidgetTestCase):
 
     def test_propgridprops01(self):
         o = pg.PGInDialogValidator()
-
 
     def test_propgridprops02(self):
         pg.PG_PROP_PASSWORD
@@ -18,7 +18,6 @@ class propgridprops_Tests(wtc.WidgetTestCase):
 
         s = p2.ValueToString(12345)
         assert s == '12345'
-
 
     def test_propgridprops03(self):
         pg.PG_PROPERTY_VALIDATION_ERROR_MESSAGE
@@ -31,25 +30,19 @@ class propgridprops_Tests(wtc.WidgetTestCase):
         pg.NumericPropertyValidator.Unsigned
         pg.NumericPropertyValidator.Float
 
-
-
     def test_propgridprops04(self):
         p = pg.IntProperty()
-
 
     def test_propgridprops05(self):
         p = pg.UIntProperty()
 
-
     def test_propgridprops06(self):
         p = pg.FloatProperty()
-
 
     def test_propgridprops07(self):
         pg.PG_PROP_USE_CHECKBOX
         pg.PG_PROP_USE_DCC
         p = pg.BoolProperty()
-
 
     def test_propgridprops08(self):
         pg.PG_PROP_STATIC_CHOICES
@@ -59,13 +52,11 @@ class propgridprops_Tests(wtc.WidgetTestCase):
                              labels=['one', 'two', 'three'],
                              values=[1, 2, 3])
 
-
     def test_propgridprops09(self):
         p1 = pg.EditEnumProperty()
         p2 = pg.EditEnumProperty(label='label', name='name',
                                  labels=['one', 'two', 'three'],
                                  values=[1, 2, 3])
-
 
     def test_propgridprops10(self):
         p1 = pg.FlagsProperty()
@@ -73,53 +64,43 @@ class propgridprops_Tests(wtc.WidgetTestCase):
                               labels=['one', 'two', 'three'],
                               values=[1, 2, 3])
 
-
     def test_propgridprops11(self):
         da = pg.PGFileDialogAdapter()
-
 
     def test_propgridprops12(self):
         pg.PG_PROP_SHOW_FULL_FILENAME
 
         p1 = pg.FileProperty()
+
         value = '/foo/bar/value'
         p2 = pg.FileProperty('label', 'name', value)
 
         fn = p2.GetFileName()
-        assert fn == value
-
+        assert fn.replace(os.path.sep, '/') == value
 
     def test_propgridprops13(self):
         pg.PG_PROP_NO_ESCAPE
         da = pg.PGLongStringDialogAdapter()
 
-
     def test_propgridprops14(self):
         p1 = pg.LongStringProperty()
 
-
     def test_propgridprops15(self):
         p1 = pg.DirProperty()
-
 
     def test_propgridprops16(self):
         p1 = pg.ArrayStringProperty()
         p2 = pg.ArrayStringProperty(label='label', name='name',
                                     value=['one', 'two', 'three'])
 
-
     def test_propgridprops17(self):
         with self.assertRaises(TypeError):
             dlg = pg.PGArrayEditorDialog(self.frame, 'message', 'caption')
             dlg.Destroy()
 
-
     def test_propgridprops18(self):
         dlg = pg.PGArrayStringEditorDialog()
         dlg.Destroy()
-
-
-
 
 
 #---------------------------------------------------------------------------
